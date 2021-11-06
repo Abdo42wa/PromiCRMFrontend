b02a384d0a7568fd5523a61caadf791240dc2a11
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-06 17:32:12 +0200

    clean up the directory and setup redux store

---
+++ promifrontend/src/store.js
@@ -0,0 +1,18 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { usersReducer } from './reducers/usersReducer';
+
+const allReducers = combineReducers({
+
+});
+
+const initialState = {
+
+}
+
+const middleware = [thunk];
+
+const store = createStore(allReducers, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+
+export default store;
