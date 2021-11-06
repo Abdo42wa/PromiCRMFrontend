a33dc26b194d4fbcfd0ed77d5ce0cec02fcca9ba
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-06 23:56:09 +0200

    import font awesome

--- promifrontend/src/Components/Header.js
+++ promifrontend/src/Components/Header.js
@@ -17,27 +17,35 @@ const Header = () => {
             <Navbar bg="dark" variant='dark' expand="lg">
                 <Container>
                     <LinkContainer to='/'>
-                        <Navbar.Brand href="#home">Primo</Navbar.Brand>
+                        <Navbar.Brand>Primo</Navbar.Brand>
                     </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <LinkContainer to='/'>
-                                <Nav.Link href="#home">Home</Nav.Link>
-                            </LinkContainer>
-                            <Nav.Link href="#link">Link</Nav.Link>
+
                             {currentUser ? (
-                                <LinkContainer to='/login'>
-                                    <Nav.Link onClick={logoutHandler} >
-                                        <i className='fas fa-user'></i> Log Out
-                                    </Nav.Link>
-                                </LinkContainer>
+                                <>
+                                    <LinkContainer to='/'>
+                                        <Nav.Link href="#home">Home</Nav.Link>
+                                    </LinkContainer>
+
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link onClick={logoutHandler} >
+                                            <i class="fas fa-sign-out-alt"></i> Log Out
+                                        </Nav.Link>
+                                    </LinkContainer>
+
+                                </>
                             ) : (
-                                <LinkContainer to='/login'>
-                                    <Nav.Link >
-                                        <i className='fas fa-user'></i> Sign In
-                                    </Nav.Link>
-                                </LinkContainer>
+                                <>
+                                    <LinkContainer to='/login'>
+                                        <Nav.Link >
+                                            <i className='fas fa-user'></i> Sign In
+                                        </Nav.Link>
+                                    </LinkContainer>
+
+
+                                </>
                             )}
                         </Nav>
                     </Navbar.Collapse>
