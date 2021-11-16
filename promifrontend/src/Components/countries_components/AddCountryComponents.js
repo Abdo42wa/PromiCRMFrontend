f43915b2c24b2501cb1c8b0caf117156b98af3d6
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-11-16 18:42:00 +0200

    update and add country component

---
+++ promifrontend/src/Components/countries_components/AddCountryComponents.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react';
+import { Modal, Button, Form, Space, Input } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
+function AddCountryComponents(props) {
+    const [country, setCountry] = useState({
+        name: "",
+        shortName: "",
+        continent: ""
+    });
+    const onCancel = () => {
+        props.onClose();
+    }
+    const onBack = () => {
+        props.onClose();
+    }
+    const onDataChange = (value, inputName) => {
+
+        setCountry(prevState => ({
+            ...prevState,
+            [inputName]: value
+        }));
+    }
+    const saveChanges = () => {
+
+        const countryClone = JSON.parse(JSON.stringify(country));
+        const postObj = {
+            "name": countryClone.name,
+            "shortName": countryClone.shortName,
+            "continent": countryClone.continent,
+
+        }
+        props.save(postObj);
+    }
+
+    return (
+        <>
+            <Modal
+                onCancel={onCancel}
+                saveChanges={saveChanges}
+                okButtonProps={{ disabled: false }}
+                cancelButtonProps={{ disabled: false }}
+                title={<Space><ArrowLeftOutlined onClick={onBack} />Pridėti naują šaly</Space>}
+                visible={props.visible}
+                footer={
+                    <div>
+                        <Button key="customCancel" onClick={onCancel}>Atšaukti</Button>
+                        <Button key="customSubmit" form="myForm" onClick={saveChanges} htmlType="submit" type={'primary'}>Pridėti</Button>
+                    </div>
+                }
+            >
+                <Form layout="vertical" id="myForm" name="myForm">
+                    <Form.Item key="name" name="name" label="Pavadinimas">
+                        <Input style={{ width: '100%' }} placeholder="Įrašykite pavadinimas" value={country.name} onChange={(e) => onDataChange(e.target.value, "name")} />
+                    </Form.Item>
+                    <Form.Item key="name2" name="name2" label="Trumpas pavadinimas	">
+                        <Input style={{ width: '100%' }} placeholder="Įrašykite trumpas pavadinimas" value={country.shortName} onChange={(e) => onDataChange(e.target.value, "shortName")} />
+                    </Form.Item>
+                    <Form.Item key="name3" name="name3" label="kontinentas">
+                        <Input style={{ width: '100%' }} placeholder="Įrašykite kontinentas" value={country.continent} onChange={(e) => onDataChange(e.target.value, "continent")} />
+                    </Form.Item>
+                </Form>
+            </Modal>
+        </>
+    )
+}
+
+export default AddCountryComponents;
---
+++ promifrontend/src/Components/countries_components/UpdateCountryComponents.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react';
+import { Modal, Button, Form, Space, Input } from 'antd';
+import { ArrowLeftOutlined } from '@ant-design/icons';
+
+const textStyle = {
+    fontSize: '18px',
+    color: '#8C8C8C',
+    fontStyle: 'normal',
+    fontWeight: 'normal',
+    lineHeight: '22px',
+    marginRight: '40px',
+    marginBottom: '4px',
+    marginTop: '10px'
+}
+function UpdateCountryComponent(props) {
+    const [country, setCountry] = useState({});
+
+    const onBack = () => {
+        props.onClose();
+    }
+    const onCancel = () => {
+        props.onClose();
+    }
+
+    const onDataChange = (value, inputName) => {
+        setCountry(prevState => ({
+            ...prevState,
+            [inputName]: value
+        }));
+    }
+    const saveChanges = () => {
+
+        const countryClone = JSON.parse(JSON.stringify(country));
+        const postObj = {
+            "name": countryClone.name,
+            "shortName": countryClone.shortName,
+            "continent": countryClone.continent,
+
+        }
+        const reducerObj = {
+            "id": countryClone.id,
+            "name": countryClone.name,
+            "shortName": countryClone.shortName,
+            "continent": countryClone.continent,
+        }
+        props.save(postObj, reducerObj);
+    }
+    useEffect(() => {
+        setCountry(props.record)
+    }, [])
+    return (
+        <>
+            <Modal
+                onCancel={onCancel}
+                saveChanges={saveChanges}
+                okButtonProps={{ disabled: false }}
+                cancelButtonProps={{ disabled: false }}
+                title={<Space><ArrowLeftOutlined onClick={onBack} />Atnaujinti šaly</Space>}
+                visible={props.visible}
+                footer={
+                    <div>
+                        <Button key="customCancel" onClick={onCancel}>Atšaukti</Button>
+                        <Button key="customSubmit" form="myForm" onClick={saveChanges} htmlType="submit" type={'primary'}>Atnaujinti</Button>
+                    </div>
+                }
+            >
+                <Form layout="vertical" id="myForm" name="myForm">
+                    <p style={{ ...textStyle }}>Pavadinimas</p>
+                    <Input style={{ width: '100%' }} placeholder="Įrašykite pavadinimas" value={country.name} onChange={(e) => onDataChange(e.target.value, "name")} />
+                    <p style={{ ...textStyle }}>Trumpas pavadinimas</p>
+                    <Input style={{ width: '100%' }} placeholder="Įrašykite trumpas pavadinimas" value={country.shortName} onChange={(e) => onDataChange(e.target.value, "shortName")} />
+                    <p style={{ ...textStyle }}>kontinentas</p>
+                    <Input style={{ width: '100%' }} placeholder="Įrašykite kontinentas" value={country.continent} onChange={(e) => onDataChange(e.target.value, "continent")} />
+                </Form>
+            </Modal>
+        </>
+    )
+}
+
+export default UpdateCountryComponent;
