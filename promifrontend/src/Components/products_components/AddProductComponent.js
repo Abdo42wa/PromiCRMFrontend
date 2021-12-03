922e006ad6474e6b5c2678b9a989d8cb807f9e05
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2021-12-03 19:27:34 +0200

    Update AddProductComponent.js

--- promifrontend/src/Components/products_components/AddProductComponent.js
+++ promifrontend/src/Components/products_components/AddProductComponent.js
@@ -3,29 +3,39 @@ import { Modal, Button, Form, Space, Input, Select } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrders } from '../../Actions/orderAction'
-import { getServices } from '../../Actions/servicesAction'
+import { getMaterialsWarehouseData } from '../../Actions/materialsWarehouseActions'
 const { Option } = Select;
 
 function AddProductComponent(props) {
     const dispatch = useDispatch();
 
     const orderReducer = useSelector((state) => state.orderReducer);
-    const servicesReducer = useSelector((state) => state.servicesReducer);
+    const materialsWarehouseReducer = useSelector((state) => state.materialsWarehouseReducer);
 
     const [products, setProduct] = useState({
         "photo": "",
         "link": "",
         "code": "",
         "category": "",
+        "productMaterials": [],
         "name": "",
         "lengthWithoutPackaging": 0,
+        "lengthWithPackaging": 0,
         "widthWithoutPackaging": 0,
+        "widthWithPackaging": 0,
         "heightWithoutPackaging": 0,
+        "heightWithPackaging": 0,
         "weightGross": 0,
+        "weightNetto": 0,
+        "collectionTime": 0,
+        "bondingTime": 0,
+        "laserTime": 0,
+        "paintingTime": 0,
+        "milingTime": 0,
         "packagingBoxCode": "",
         "packingTime": 0,
-        "serviceId": 0,
-        "orderId": 0
+        "orderId": 0,
+
     });
 
 
@@ -37,9 +47,11 @@ function AddProductComponent(props) {
     }
     const onDataChange = (value, inputName) => {
 
-        if (inputName === 'lengthWithoutPackaging' || inputName === 'serviceId' || inputName === "orderId" ||
-            inputName === 'widthWithoutPackaging' || inputName === 'heightWithoutPackaging' ||
-            inputName === 'weightGross' || inputName === 'packingTime') {
+        if (inputName === 'lengthWithoutPackaging' || inputName === 'lengthWithPackaging' || inputName === "orderId" ||
+            inputName === 'widthWithoutPackaging' || inputName === 'widthWithPackaging' || inputName === 'heightWithoutPackaging' || inputName === 'heightWithPackaging' ||
+            inputName === 'weightGross' || inputName === 'weightNetto' || inputName === 'packingTime'
+            || inputName === 'collectionTime' || inputName === 'bondingTime' || inputName === 'laserTime'
+            || inputName === 'paintingTime' || inputName === 'milingTime') {
             setProduct(prevState => ({
                 ...prevState,
                 [inputName]: Number(value)
@@ -49,12 +61,19 @@ function AddProductComponent(props) {
                 ...prevState,
                 [inputName]: value
             }))
-            console.log(value);
+
         }
     }
     const saveChanges = () => {
         const dataProduct = JSON.parse(JSON.stringify(products));
-
+        const array = [];
+        // console.log("heheheheheheh" + JSON.stringify(productMaterials))
+        dataProduct.productMaterials.map((element, index) => {
+            const obj = {
+                "materialWarehouseId": element
+            }
+            array.push(obj)
+        })
         console.log(JSON.parse(JSON.stringify(products)))
         const postObj = {
             "photo": dataProduct.photo,
@@ -68,17 +87,26 @@ function AddProductComponent(props) {
             "weightGross": dataProduct.weightGross,
             "packagingBoxCode": dataProduct.packagingBoxCode,
             "packingTime": dataProduct.packingTime,
-            "serviceId": dataProduct.serviceId,
             "orderId": dataProduct.orderId,
+            "heightWithPackaging": dataProduct.heightWithPackaging,
+            "widthWithPackaging": dataProduct.widthWithPackaging,
+            "lengthWithPackaging": dataProduct.lengthWithPackaging,
+            "weightNetto": dataProduct.weightNetto,
+            "collectionTime": dataProduct.collectionTime,
+            "bondingTime": dataProduct.bondingTime,
+            "paintingTime": dataProduct.paintingTime,
+            "laserTime": dataProduct.laserTime,
+            "milingTime": dataProduct.milingTime,
+            "productMaterials": array
 
         }
         props.save(postObj);
-        console.log(postObj);
+        console.log(JSON.stringify(postObj));
     }
     useEffect(() => {
 
         dispatch(getOrders(() => {
-            dispatch(getServices())
+            dispatch(getMaterialsWarehouseData())
         }));
     }, [dispatch]);
 
@@ -118,32 +146,61 @@ function AddProductComponent(props) {
                     <Form.Item key="name6" name="name6" label="Produkto ilgis Be pakuotės">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite ilgis Be pakuotės" value={products.lengthWithoutPackaging} onChange={(e) => onDataChange(e.target.value, "lengthWithoutPackaging")} />
                     </Form.Item>
-                    <Form.Item key="name7" name="name7" label="Produkto plotis Be pakuotės">
+                    <Form.Item key="name7" name="name7" label="Produkto ilgis su pakuotės">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite ilgis su pakuotės" value={products.lengthWithPackaging} onChange={(e) => onDataChange(e.target.value, "lengthWithPackaging")} />
+                    </Form.Item>
+                    <Form.Item key="name8" name="name8" label="Produkto plotis Be pakuotės">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite plotis Be pakuotė" value={products.widthWithoutPackaging} onChange={(e) => onDataChange(e.target.value, "widthWithoutPackaging")} />
                     </Form.Item>
-                    <Form.Item key="name8" name="name8" label="Produkto aukštis Be pakuotės">
+                    <Form.Item key="name9" name="name9" label="Produkto plotis su pakuotės">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite plotis su pakuotė" value={products.widthWithPackaging} onChange={(e) => onDataChange(e.target.value, "widthWithPackaging")} />
+                    </Form.Item>
+                    <Form.Item key="name10" name="name10" label="Produkto aukštis Be pakuotės">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite aukštis Be pakuotės" value={products.heightWithoutPackaging} onChange={(e) => onDataChange(e.target.value, "heightWithoutPackaging")} />
                     </Form.Item>
-                    <Form.Item key="name9" name="name9" label="Produkto svoris Bruto">
+                    <Form.Item key="name11" name="name11" label="Produkto aukštis su pakuotės">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite aukštis su pakuotės" value={products.heightWithPackaging} onChange={(e) => onDataChange(e.target.value, "heightWithPackaging")} />
+                    </Form.Item>
+                    <Form.Item key="name12" name="name12" label="Produkto svoris Bruto">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite svoris Bruto" value={products.weightGross} onChange={(e) => onDataChange(e.target.value, "weightGross")} />
                     </Form.Item>
-                    <Form.Item key="name10" name="name10" label="Pakuotės dėžutės kodas">
+                    <Form.Item key="name13" name="name13" label="Produkto svoris Netto">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite svoris Netto" value={products.weightNetto} onChange={(e) => onDataChange(e.target.value, "weightNetto")} />
+                    </Form.Item>
+                    <Form.Item key="name14" name="name14" label="Pakuotės dėžutės kodas">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite dėžutės kodas" value={products.packagingBoxCode} onChange={(e) => onDataChange(e.target.value, "packagingBoxCode")} />
                     </Form.Item>
-                    <Form.Item key="name11" name="name11" label="Pakavimo laikas">
+                    <Form.Item key="name15" name="name15" label="Pakavimo laikas">
                         <Input required style={{ width: '100%' }} placeholder="Įrašykite pakavimo laikas" value={products.packingTime} onChange={(e) => onDataChange(e.target.value, "packingTime")} />
                     </Form.Item>
+                    <Form.Item key="name16" name="name16" label="surinkimo laikas">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite surinkimo laikas" value={products.collectionTime} onChange={(e) => onDataChange(e.target.value, "collectionTime")} />
+                    </Form.Item>
+                    <Form.Item key="name17" name="name17" label="Suklijavimo laikas">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite Suklijavimo laikas" value={products.bondingTime} onChange={(e) => onDataChange(e.target.value, "bondingTime")} />
+                    </Form.Item>
+                    <Form.Item key="name18" name="name18" label="Lazeriavimo laikas">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite Lazeriavimo laikas" value={products.laserTime} onChange={(e) => onDataChange(e.target.value, "laserTime")} />
+                    </Form.Item>
+                    <Form.Item key="name19" name="name19" label="Dažymo laikas">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite Dažymo laikas" value={products.paintingTime} onChange={(e) => onDataChange(e.target.value, "paintingTime")} />
+                    </Form.Item>
+                    <Form.Item key="name20" name="name20" label="Frezavimo laikas">
+                        <Input required style={{ width: '100%' }} placeholder="Įrašykite Frezavimo laikas" value={products.milingTime} onChange={(e) => onDataChange(e.target.value, "milingTime")} />
+                    </Form.Item>
 
-                    <p style={{ marginBottom: '5px' }}>Paslaugos </p>
+                    <p style={{ marginBottom: '5px' }}>Mazegos </p>
                     <Select
                         showSearch
+                        mode="multiple"
+                        allowClear
                         style={{ width: '320px' }}
                         placeholder="Priskirkite paslaugos"
                         optionFilterProp="children"
-                        onChange={(e) => onDataChange(e, "serviceId")}
+                        onChange={(e) => onDataChange(e, "productMaterials")}
                     >
-                        {servicesReducer.services.map((element, index) => {
-                            return (<Option key={element.id} value={element.id}>{element.name}</Option>)
+                        {materialsWarehouseReducer.materialsWarehouseData.map((element, index) => {
+                            return (<Option key={element.id} value={element.id}>{element.title}</Option>)
                         })}
                     </Select>
 
