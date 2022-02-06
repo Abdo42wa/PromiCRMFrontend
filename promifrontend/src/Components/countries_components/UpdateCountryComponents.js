bf355e5fae5497a0e093b733ba9b51ab299228a3
abdo42wa <abdelrahman-ahmed-ali-mohamed.elsayed@stud.viko.lt>
2022-02-06 15:38:23 +0200

    Update UpdateCountryComponents.js

--- promifrontend/src/Components/countries_components/UpdateCountryComponents.js
+++ promifrontend/src/Components/countries_components/UpdateCountryComponents.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { Modal, Button, Form, Space, Input } from 'antd';
+import { Modal, Button, Form, Space, Input, Select } from 'antd';
 import { ArrowLeftOutlined } from '@ant-design/icons';
+import { countries } from './countriesData'
 
 const textStyle = {
     fontSize: '18px',
@@ -12,6 +13,7 @@ const textStyle = {
     marginBottom: '4px',
     marginTop: '10px'
 }
+const { Option } = Select;
 function UpdateCountryComponent(props) {
     const [country, setCountry] = useState({});
 
@@ -23,13 +25,25 @@ function UpdateCountryComponent(props) {
     }
 
     const onDataChange = (value, inputName) => {
-        setCountry(prevState => ({
-            ...prevState,
-            [inputName]: value
-        }));
+        // setCountry(prevState => ({
+        //     ...prevState,
+        //     [inputName]: value
+        // }));
+
+        if (inputName === "name") {
+            let obj = countries.find(x => x.name === value)
+            // console.log(obj.code)
+            console.log(country.shortName)
+            setCountry(prevState => ({
+                ...prevState,
+                [inputName]: value,
+                "shortName": obj.code
+
+            }));
+        }
     }
     const saveChanges = () => {
-        const {id,...postObj} = country;
+        const { id, ...postObj } = country;
         const reducerObj = country;
         props.save(postObj, reducerObj);
     }
@@ -55,7 +69,19 @@ function UpdateCountryComponent(props) {
             >
                 <Form layout="vertical" id="myForm" name="myForm">
                     <p style={{ ...textStyle }}>Pavadinimas</p>
-                    <Input required style={{ width: '100%' }} placeholder="Įrašykite pavadinimas" value={country.name} onChange={(e) => onDataChange(e.target.value, "name")} />
+                    {/* <Input required style={{ width: '100%' }} placeholder="Įrašykite pavadinimas" value={country.name} onChange={(e) => onDataChange(e.target.value, "name")} /> */}
+
+                    <Select
+                        style={{ width: '100%' }}
+                        showSearch
+                        id="country"
+                        value={country.name}
+                        onChange={(e) => onDataChange(e, "name")}
+                    >
+                        {countries.map((element, index) => {
+                            return (<Option key={index} value={element.name}>{element.name}</Option>)
+                        })}
+                    </Select>
                     <p style={{ ...textStyle }}>Trumpas pavadinimas</p>
                     <Input required style={{ width: '100%' }} placeholder="Įrašykite trumpas pavadinimas" value={country.shortName} onChange={(e) => onDataChange(e.target.value, "shortName")} />
                 </Form>
